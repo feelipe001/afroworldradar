b10a4f43093fcffb09fd58f514509279bc9ebf95
feelipe001 <lypanfelipe@gmail.com>
2025-05-27 09:30:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-
 function searchMovie() {
-  const query = document.getElementById('searchInput').value;
-  if (!query) {
-    alert('Digite o nome de um filme!');
-    return;
-  }
-  document.getElementById('results').innerHTML = '<p>Simulação: resultado para "' + query + '"</p>';
+    const query = document.getElementById('searchInput').value;
+    if (!query) {
+        alert('Digite o nome de um filme!');
+        return;
+    }
+
+    document.getElementById('results').innerHTML = "<p>Simulação: resultado para '" + query + "'</p>";
+}
+
+// Função do botão ASSISTIR com Popunder
+function watch(title) {
+    // Abre o popunder (anúncio)
+    window.open('https://pl26763948.profitableratecpm.com/40/cf/b5/40cfb5facfa5de3009ac9eb0de7713c4', '_blank');
+
+    // Salva o nome do filme e redireciona
+    localStorage.setItem("movieTitle", title);
+    window.location.href = "assistir.html";
 }
